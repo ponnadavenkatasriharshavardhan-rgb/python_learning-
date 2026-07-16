d346d6b72312aecaa973062cab93e9d93fb83548
ponnadavenkatasriharshavardhan-rgb <ponnadavenkatasriharshavardhan@gmail.com>
2026-07-16 20:50:26 +0530

    Add files via upload

---
+++ day 1 what is python.py
@@ -0,0 +1,105 @@
+'''
+
+python is released in 1991 by russom
+
+what is python?
+------------------------
+high level language
+OOPs
+interpreted language
+-----------------------
+the code will be executed line by line that's the reason python interpreted language
+dynamically typed language
+-------------------------
+no need to mention the type of data pass to the variable
+
+
+why python?
+-----------------
+easy to learn
+less syntax
+easy understand
+cross platform
+open source
+large num of library
+ 
+
+comments
+-------------------
+single line (#)
+this is used to explain line in the code
+
+multi line
+used to comment multiple line using ''' ''' , """ """
+
+
+
+variables
+-------------------
+this is used to store the datatypes
+
+good way to define
+--------------------
+_num
+num or NUM
+
+bad way to define
+--------------------
+#$% , space ,no
+
+
+Boolean type
+--------------------
+the boolean type in python represents one of two values
+
+num = 9
+num_2 = 89
+print(num != num_2)
+
+num = num_2 = num_3 = 78
+print(num_2)
+
+output
+true
+78
+
+multiple variables , single values
+------------------------------------------
+you can assign the same values to multiple variables in a single line
+
+num =num_2 = 90
+print (num)
+print (num_2)
+
+output
+90
+90
+
+multiple variables , multiple values
+------------------------------------------
+you can assign different values to multiple variables in a single line
+
+num,num_2 = 9,90
+print(num)
+print (num_2)
+
+output
+9
+90
+
+swapping of two variables
+---------------------------
+swapping means exchanging the values of two variables 
+
+num =7
+num_2 = 70
+num , num_2 = num_2 , num
+# 7, 70 = 70 , 7
+print(num)
+
+output
+70
+
+
+'''
+

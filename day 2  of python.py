c138058e2956a6897e0e2b2e56985cb0138c3a6d
ponnadavenkatasriharshavardhan-rgb <ponnadavenkatasriharshavardhan@gmail.com>
2026-07-17 12:40:08 +0530

    day 2 of python
    tokens - keywords, identifiers, operators, literals and types of operators

---
+++ day 2  of python.py
@@ -0,0 +1,148 @@
+'''
+
+  tokens
+-------------
+
+keywords
+------------
+if
+for
+else
+return
+while
+and
+in
+
+identifires
+---------------
+variables
+function name
+class name
+
+operators
+-------------
++ - =
+
+literal
+-----------
+int,str,tuple,list
+
+
+operators
+--------------
+1 arithamatic
+---------------
+-,+,/,%,*,**
+ex
+num =8
+num_2 =7
+print (num + num_2)
+print (num - num_2)
+print (num * num_2)
+print (num ** num_2)
+print (num % num_2)
+print (num / num_2)
+
+output
+------------
+15
+1
+56
+2097152
+1
+1.1428571428571428
+
+2 assignment
+------------
+=+=,-=,*=,%=,/=
+
+3 comparision
+----------------
+==,!=,<=,>=,>,<
+
+num = 8
+num_2 = 7
+print(num == num_2)
+print (num != num_2)
+print (num > num_2)
+print (num < num_2)
+print (num >= num_2)
+print (num <= num_2)
+
+output
+----------
+False
+True
+True
+False
+True
+False
+
+4 logical
+-------------
+and or not
+
+num = 78
+num_2 = 89
+print (num < num_2 and num_2 < 100)
+print (num > num_2 or num_2 < 100)
+print (not (num < num_2 and num_2 < 100))
+
+output
+-----------
+True
+True
+False
+
+5 identity
+------------
+ is
+ is not
+
+so = 12
+how = 13
+print (id (so))
+print(id (how))
+print (so is how )
+print (so is not how)
+
+output
+---------------
+140714074195208
+140714074195240
+False
+True
+
+6 membership
+----------------
+in
+in not
+
+so = (1,2,3,4,5)
+print (7 not in so)
+
+output
+----------
+True
+
+7 bitwise
+-------------
+&, |, ^, <<,>>
+
+print (5 & 4)
+print (5 | 4)
+print (5 >> 4)
+print (5 << 4)
+
+output
+--------------
+4
+5
+0
+80
+
+
+'''
+
+
+
